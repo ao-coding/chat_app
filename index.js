5129d8096bd84b45051e01777d8ff646ca008c8d
aomeri <117003825+aomeri@users.noreply.github.com>
2022-11-02 12:19:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+const cors = require('cors')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
